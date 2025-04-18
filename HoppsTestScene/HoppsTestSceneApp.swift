e21b3fd5b5c1efb5ba03a30fe40f57485a97c0a3
rayaanismail <1rayaan.ismail@gmail.com>
2025-04-17 22:42:05 -0400

    Initial Commit

---
+++ HoppsTestScene/HoppsTestSceneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoppsTestSceneApp.swift
+//  HoppsTestScene
+//
+//  Created by Rayaan Ismail on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct HoppsTestSceneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HoppsTestSceneTests/HoppsTestSceneTests.swift
@@ -0,0 +1,17 @@
+//
+//  HoppsTestSceneTests.swift
+//  HoppsTestSceneTests
+//
+//  Created by Rayaan Ismail on 4/17/25.
+//
+
+import Testing
+@testable import HoppsTestScene
+
+struct HoppsTestSceneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
