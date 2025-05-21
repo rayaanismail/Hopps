89b43f9feb5438d7ba4708aba75575e3f4d71f40
rayaanismail <1rayaan.ismail@gmail.com>
2025-05-21 13:45:11 -0400

    Readme file for ui todo

---
+++ Sources/UI/README.swift
@@ -0,0 +1,17 @@
+//
+//  README.swift
+//  HoppsTestScene
+//
+//  Created by Rayaan Ismail on 5/21/25.
+//
+
+import Foundation
+
+// Main Starting Screen with 4 buttons,
+/// Leaderboard, settings, start, and skins
+///
+
+// Build out the pause screen with 3 buttons
+/// Resume, settings home
+
+// Settings screen with the options to disable and enable vibration, sounds and touch/tilt control
